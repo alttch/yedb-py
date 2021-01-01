ba7887285a001d7adc28c77fe4e9399bcfd6bc70
Sergei S <div@altertech.com>
2021-01-01 17:26:57 +0100

    0.0.23

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.0.22
+VERSION=0.0.23
 
 all:
 	@echo "Select target"
--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '0.0.22'
+__version__ = '0.0.23'
 
 # -*- coding: utf-8 -*-
 #
