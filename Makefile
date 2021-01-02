3199c1ec096f0c0a0c0bb2d03e07187893263038
Sergei S <div@altertech.com>
2021-01-02 10:25:11 +0100

    0.0.32

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.0.31
+VERSION=0.0.32
 
 all:
 	@echo "Select target"
--- setup-server.sh
+++ setup-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-YEDB_VERSION=0.0.31
+YEDB_VERSION=0.0.32
 
 REQUIRED="realpath python3 curl"
 MODS="yedb==${YEDB_VERSION} msgpack==1.0.2 aiohttp==3.7.3"
