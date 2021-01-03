5637aceac9beaae74ce45dc7faba2521a557a244
Sergei S <div@altertech.com>
2021-01-03 07:48:24 +0100

    0.0.38

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.0.37
+VERSION=0.0.38
 
 all:
 	@echo "Select target"
--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '0.0.37'
+__version__ = '0.0.38'
 
 # -*- coding: utf-8 -*-
 #
