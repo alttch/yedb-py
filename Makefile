309dfb14de450127cc06c1328d87ec9ec7de8f78
Sergei S <div@altertech.com>
2021-01-04 10:29:23 +0100

    v0.0.45

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.0.44
+VERSION=0.0.45
 
 all:
 	@echo "Select target"
--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '0.0.44'
+__version__ = '0.0.45'
 
 # -*- coding: utf-8 -*-
 #
