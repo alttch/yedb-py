8ccd3d18c31aacba4732a54d226eda49079404c6
Sergei S <div@altertech.com>
2021-10-31 21:00:54 +0100

    common

---
+++ yedb/common.py
@@ -0,0 +1,10 @@
+METHODS = [
+    'test', 'key_get', 'key_set', 'key_list', 'key_list_all',
+    'key_get_recursive', 'key_copy', 'key_rename', 'key_exists', 'key_explain',
+    'key_delete', 'key_delete_recursive', 'purge', 'check', 'repair', 'info',
+    'key_dump', 'key_load', 'key_update', 'safe_purge', 'purge_cache',
+    'server_set', 'key_increment', 'key_decrement', 'key_get_field',
+    'key_set_field', 'key_delete_field'
+]
+
+DEFAULT_SERVER_PORT = 8870
