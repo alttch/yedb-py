35e5ba83db28702e79a949c5feda3e00b8417925
Sergei S <div@altertech.com>
2021-10-31 21:17:07 +0100

    fmt

--- doc/generate-pydocs.py
+++ doc/generate-pydocs.py
@@ -8,8 +8,8 @@ import sh
 if len(sys.argv) < 2:
     for i in glob.glob('pydoc/tpl_*.rst'):
         out = i.replace('pydoc/tpl_', 'pydoc/pydoc_')
-        os.system(f'pydoc2rst {i} {out}')
+        os.system(f'pydoc2rst {i} {out} /opt/yedb-py')
 else:
     for i in sys.argv[1:]:
         os.system(
-            f'pydoc2rst pydoc/tpl_{i}.rst pydoc/pydoc_{i}.rst /opt/yedb')
+            f'pydoc2rst pydoc/tpl_{i}.rst pydoc/pydoc_{i}.rst /opt/yedb-py')
