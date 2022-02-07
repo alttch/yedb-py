5103fd18c7970d5a68f3c48ef9a07843b6fc43bb
Sergei S <div@altertech.com>
2022-02-07 01:10:03 +0100

    0.2.19

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = '0.2.18'
+__version__ = '0.2.19'
 
 # -*- coding: utf-8 -*-
 #
--- yedb/__init__.py
+++ yedb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.2.18'
+__version__ = '0.2.19'
 
 DB_VERSION = 1
 
